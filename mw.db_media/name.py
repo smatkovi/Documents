d6ccc0ded6f5f2271d3c97cf0afbae3d659c73b1
sebastian <sebastian.matkovich@gmail.com>
2019-09-11 01:43:03 +0200

    removed unnecessary files

--- mw.db_media/name.py
+++
@@ -1,8 +0,0 @@
-import csv
-import numpy as np
-with open("media",'r') as dest_f:
-        data_iter = csv.reader(dest_f, delimiter =' ', quotechar = '"')
-        data = [data for data in data_iter]
-        data_array = np.asarray(data, dtype = str) 
-
-        print ', '.join(data_array)
--- mw.db_media/name2.py
+++
@@ -1,9 +0,0 @@
-s=open("media", 'r');
-l=s.read().split('"')[1::2];
-e=l[0:][::2];
-o=l[1:][::2];
-for i in range(len(e)):
-  print 'mv ';
-  print e[i];
-  print o[i];
-  print ';';
